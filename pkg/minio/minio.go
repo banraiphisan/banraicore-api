fc9b04aa701f68229ad49f1f2981e7b31a09a699
theeraprasert meemui <tub@theerapraserts-MacBook-Pro.local>
2025-05-01 05:48:46 +0700

    ci: test workflow

--- pkg/minio/minio.go
+++ pkg/minio/minio.go
@@ -2,6 +2,7 @@ package minio
 
 import (
 	"context"
+	"crypto/tls"
 	"fmt"
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
@@ -39,7 +40,8 @@ func New(cfg config.MinioConfig) (*MinioClient, error) {
 		Creds:  credentials.NewStaticV4(cfg.AccessKey, cfg.SecretKey, ""),
 		Secure: cfg.UseSSL,
 		Transport: &http.Transport{
-			Proxy: http.ProxyFromEnvironment,
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+			Proxy:           http.ProxyFromEnvironment,
 		},
 	}
 	client, err := minio.New(sanitizedEndpoint, &options)
